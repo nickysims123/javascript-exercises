f6f903c00672bb196d4c2f635b3aac64a121b43b
Nicholas Sima <nick.sima@outlook.com>
2024-07-02 14:11:36 -0400

    Finished 05-sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    for(let j = 0; j < args.length; j++) {
+        console.log(args[j]);
+        if (!(Number.isInteger(args[j])) || args[j] < 0) return 'ERROR';
+    }
+    let sum = 0;
 
+    for (let i = 0; i < Math.max(...args); i++) {
+        sum = sum + Math.min(...args) + i;
+    }
+
+    return sum;
 };
+
+sumAll(-24,10);
 
 // Do not edit below this line
 module.exports = sumAll;

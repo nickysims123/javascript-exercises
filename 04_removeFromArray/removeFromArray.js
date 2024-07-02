8b54b8e96d998049231a5f2205bb97bd16435957
Nicholas Sima <nick.sima@outlook.com>
2024-07-02 11:23:40 -0400

    Finished 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    let result = [];
+
+    arr.forEach(element => {
+        if (!nums.includes(element)) {
+            result.push(element);
+        }
+    });
+
+    return result;
 };
 
 // Do not edit below this line

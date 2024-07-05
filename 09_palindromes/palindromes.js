f24cca6e12dbb07d86a2bf1f0f08035efa50d446
Nicholas Sima <nick.sima@outlook.com>
2024-07-05 16:42:59 -0400

    Skipped 09-palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let i = 0, j = string.length - 1;
 
+    for(i; i <= string.length / 2; i++) {
+        if (string.slice(i, i+1) != string.slice(j, j+1)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line

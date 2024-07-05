b814e796614a49ef937940ee3d69f382b11955bb
Nicholas Sima <nick.sima@outlook.com>
2024-07-05 16:25:50 -0400

    Finished 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(...args) {
+  return args.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0)
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return args.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let result = 1;
+  for (let i = 0; i < power; i++) {
+    result = result * base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(arg) {
+  let result = 1;
+	for (let i = 1; i <= arg; i++) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
